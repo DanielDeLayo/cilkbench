753ea07541539f31e147a97292cf07489e8c9fca
Daniel DeLayo <dandelayo@gmail.com>
2025-02-24 13:28:38 -0500

    cilk5 results parsing

--- parseResults.py
+++ parseResults.py
@@ -1,21 +1,47 @@
 import pandas as pd
+import re
 
-
+ntrials=2
 compilers = ["cilkiaf", "tapir"]
 workers = ["1", "24", "48"]
-programs = ["cholesky", "cilksort", "fft", "heat", "lu", "matmul", "nqueens", "qsort", "rectmul", "strassen"]
+runtime_pattern = re.compile("^\d+\.\d+$")
 
-# LAYOUT: Compilers, Programs, Workers
-index = pd.MultiIndex.from_product([compilers, programs, workers],names=["Compiler", "Program", "Worker"])
 
-data = []
+# Returns a list of the running time for the given compiler, worker, program tuple
+def parse_file(subdir, c, w, p):
+  target = subdir + "/Run-" + "-".join([c,w,p]) + ".txt"
+  results = []
+  
+  with open(target, "r") as f:
+    for line in f:
+      match = runtime_pattern.findall(line)
+      if match:
+        results.append(match[0])
+  
+  #print(target)
+  #print(results)
+    
+  return results
+  
+  
 
-for c in compilers:
-  for p in programs:
-    for w in workers:
-      data.append((c + p + w + "A", c + p + w + "B"))
+def cilk5_gather():
+  programs = ["cholesky", "cilksort", "fft", "heat", "lu", "matmul", "nqueens", "qsort", "rectmul", "strassen"]
 
-df = pd.DataFrame(data, index)
+  # LAYOUT: Compilers, Programs, Workers
+  index = pd.MultiIndex.from_product([compilers, programs, workers],names=["Compiler", "Program", "Worker"])
 
-print(df)
+  data = []
+
+  for c in compilers:
+    for p in programs:
+      for w in workers:
+        data.append(parse_file("cilk5", c, p, w))
+
+  df = pd.DataFrame(data, index)
+
+  print(df)
+
+
+cilk5_gather()
 

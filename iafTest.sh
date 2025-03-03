1c261a919879fc99056d78456621df39aaaee569
Daniel DeLayo <ddelayo@cs.stonybrook.edu>
2025-03-03 13:58:03 -0500

    remove old data on run

--- iafTest.sh
+++ iafTest.sh
@@ -2,5 +2,6 @@
 
 export CILKIAF_CACHE=10000
 
+rm cilk5/Run*
 bash regressionTest.sh -t -iaf -w=48,24,1
 
--- raceTest.sh
+++ raceTest.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 #bash regressionTest.sh -t -prace -csan -cprace -w=48,24,1
+rm cilk5/Run*
 bash regressionTest.sh -cprace -prace -csan -t -w=48,24,1
 

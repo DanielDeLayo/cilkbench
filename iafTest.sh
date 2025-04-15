2332e76ed4ed2691f934d0edf887b5ecfac127f6
Daniel DeLayo <ddelayo@cs.stonybrook.edu>
2025-04-15 11:53:54 -0400

    single threaded IAF testing for sampling

--- iafTest.sh
+++ iafTest.sh
@@ -3,6 +3,7 @@
 export CILKIAF_CACHE=65536
 
 rm cilk5/Run*
-bash regressionTest.sh -t -iaf -w=48,24,1
+#bash regressionTest.sh -t -iaf -w=48,24,1
+bash regressionTest.sh -t -iaf -w=1
 tar -czf results.tar.gz cilk5/Run*
 
